ab3fcca20c2c55b1f6ef5809c706a378c1595ca4
Kalinda Jayasinghe <52993668+KalindaJ-debug@users.noreply.github.com>
2023-04-17 15:02:03 +0530

    Final push code

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomepageComponent } from './modules/homepage/homepage/homepage.component';
 
 const routes: Routes = [
-  {
-    component:HomepageComponent,
-    path: ''
-  },
   {
     path: 'auth',
     loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
